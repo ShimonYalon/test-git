ac0d5d89b47cf3ce74be3959849c7b20f9c0c801
ShimonYalon <119320595+ShimonYalon@users.noreply.github.com>
2023-07-10 23:40:15 +0300

    addrd "mySecretStruct"

--- kernel.cpp
+++ kernel.cpp
@@ -1,7 +1,11 @@
 #include "kernel.hpp" // note: unbalanced round brackets () are not allowed and string literals can't be arbitrarily long, so periodically interrupt with )+R(
 string opencl_c_container() { return R( // ########################## begin of OpenCL C code ####################################################################
 
-
+extern "c"{
+typedef struct{
+char c;
+	 }mySecretStruct;
+			    }
 
 kernel void add_kernel(global float* A, global float* B, global float* C) { // equivalent to "for(uint n=0u; n<N; n++) {", but executed in parallel
 	const uint n = get_global_id(0);
